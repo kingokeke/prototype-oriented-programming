c0b4976bb992ced45bc68cb9e7fb82181ad4b321
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-26 14:37:32 +0100

    Adds test to check that only strings are passed as arguments of User

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,9 +5,12 @@ var User = require('../user');
 
 describe('User', function() {
 	it('should instantiate a user object', function() {
-		var name = 'Kingsley';
-		var email = 'kingsley@mail.com';
-		var password = '1961';
-		expect(new User(name, email, password)).toBeDefined();
+		expect(new User('Kingsley', 'kingsley@mail.com', '1961')).toBeDefined();
+	});
+
+	it('should accept only strings as arguments', function() {
+		expect(function() {
+			new User('Kingsley', 'kingsley@mail.com', 1961);
+		}).toThrow();
 	});
 });
