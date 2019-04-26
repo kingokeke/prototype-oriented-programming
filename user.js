cd87f1f1f229826bb968278be7169d7c9ab2d0c8
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-04-26 14:43:42 +0100

    Adds functionality to ensure that arguments of User are strings only

--- user.js
+++ user.js
@@ -1,6 +1,9 @@
 var User = (function() {
 	var DB = require('./db');
 	function User(name, email, password) {
+		if (typeof name !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+			throw new Error('Invalid arguments. Please enter only strings');
+		}
 		this.name = name;
 		this.email = email;
 		this.password = password;
