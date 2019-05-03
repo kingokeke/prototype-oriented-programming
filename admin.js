fe9b3ef5dc822f7a55189dfd739d1354fc2b77bb
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-05-03 14:02:16 +0100

    Adds functionality to block deleted admins from accessing deleteUser

--- admin.js
+++ admin.js
@@ -20,6 +20,11 @@ var Admin = (function() {
 		return DB.users;
 	};
 	Admin.prototype.deleteUser = function(String_userID) {
+		if (DB.users[this.userID].isActive === false) {
+			console.log('Your account has been disabled. Please contact an admin for further assistance.');
+			throw new Error('Your account has been disabled. Please contact an admin for further assistance.');
+		}
+
 		if (DB.users.hasOwnProperty(String_userID)) {
 			DB.users[String_userID].isActive = false;
 			console.log(DB.users[String_userID]);
