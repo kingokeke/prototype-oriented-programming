c6e603c8bf29535036234921ef3e0670ad18b8d4
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-05-03 17:23:44 +0100

    Refactors Admin to make code cleaner

--- admin.js
+++ admin.js
@@ -11,34 +11,44 @@ var Admin = (function() {
 	Admin.prototype = Object.create(User.prototype, {constructor: {value: Admin}});
 
 	Admin.prototype.readAllUsers = function() {
+		var errorMessage;
+
 		if (DB.users[this.userID].isActive === false) {
-			console.log('Your account has been disabled. Please contact an admin for further assistance.');
-			throw new Error('Your account has been disabled. Please contact an admin for further assistance.');
+			errorMessage = 'Your account has been disabled. Please contact an admin for further assistance.';
+			console.log(errorMessage);
+			throw new Error(errorMessage);
 		}
 
 		console.log(DB.users);
 		return DB.users;
 	};
+
 	Admin.prototype.deleteUser = function(String_userID) {
+		var errorMessage;
+
 		if (DB.users[this.userID].isActive === false) {
-			console.log('Your account has been disabled. Please contact an admin for further assistance.');
-			throw new Error('Your account has been disabled. Please contact an admin for further assistance.');
+			errorMessage = 'Your account has been disabled. Please contact an admin for further assistance.';
+			console.log(errorMessage);
+			throw new Error(errorMessage);
 		}
 
 		if (!DB.users.hasOwnProperty(String_userID)) {
-			console.log('The user with user ID: ' + String_userID + ' does not exist in the database.');
-			throw new Error('The user with user ID: ' + String_userID + ' does not exist in the database.');
+			errorMessage = 'The user with user ID: ' + String_userID + ' does not exist in the database.';
+			console.log(errorMessage);
+			throw new Error(errorMessage);
 		}
 
 		if (DB.users[String_userID].isActive === false) {
-			console.log('The user with user ID: ' + String_userID + ' has been deleted already.');
-			throw new Error('The user with user ID: ' + String_userID + ' has been deleted already.');
+			errorMessage = 'The user with user ID: ' + String_userID + ' has been deleted already.';
+			console.log(errorMessage);
+			throw new Error(errorMessage);
 		}
 
 		DB.users[String_userID].isActive = false;
 		console.log(DB.users[String_userID]);
 		return DB.users[String_userID];
 	};
+
 	Admin.prototype.deleteAllUsers = function() {};
 	return Admin;
 })();
