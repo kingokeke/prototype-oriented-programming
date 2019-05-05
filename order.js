1b87430157790e699eb238dd21a2889f0099ddab
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-05-05 19:39:33 +0100

    Adds code to increment orderCount and populate DB with order details

--- order.js
+++ order.js
@@ -1,7 +1,22 @@
 var Order = (function() {
+	var DB = require('./db');
 	function Order(String_userID, Array_products) {
+		var timestamp = new Date();
 		this.userID = String_userID;
+		this.orderID = 'order-' + String(DB.orderCount + 1);
+		this.date = timestamp.toDateString();
+		this.time = timestamp.toLocaleTimeString();
 		this.products = Array_products;
+
+		DB.orders[this.orderID] = {
+			orderID: this.orderID,
+			userID: this.userID,
+			date: this.date,
+			time: this.time,
+			products: this.products,
+		};
+
+		DB.orderCount++;
 	}
 	return Order;
 })();
--- tests/order.test.js
+++ tests/order.test.js
@@ -10,7 +10,8 @@ describe('Order constructor', function() {
 	});
 	it('should increment the order count', function() {
 		var initialOrderCount = DB.orderCount;
-		new Order('user-1', ['tea', 'bread', 'butter']);
+		var order1 = new Order('user-1', ['tea', 'bread', 'butter']);
+		console.log(order1);
 		expect(DB.orderCount).toBe(initialOrderCount + 1);
 	});
 });
