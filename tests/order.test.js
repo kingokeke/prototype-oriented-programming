5b4b528a49cfcba0717922ab576477943898b876
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-05-05 19:44:28 +0100

    Checks if Order populates the DB with order details when instantiated

--- tests/order.test.js
+++ tests/order.test.js
@@ -6,12 +6,19 @@ var Order = require('../order');
 
 describe('Order constructor', function() {
 	it('should exist', function() {
-		expect(new Order('user-1', ['tea', 'bread', 'butter'])).toBeDefined();
+		var order1 = new Order('user-1', ['tea', 'bread', 'butter']);
+		expect(order1).toBeDefined();
 	});
+
 	it('should increment the order count', function() {
 		var initialOrderCount = DB.orderCount;
-		var order1 = new Order('user-1', ['tea', 'bread', 'butter']);
-		console.log(order1);
+		var order2 = new Order('user-1', ['tea', 'bread', 'butter']);
 		expect(DB.orderCount).toBe(initialOrderCount + 1);
+	});
+
+	it('should populate the database', function() {
+		var order3 = new Order('user-1', ['tea', 'bread', 'butter']);
+		console.log(order3);
+		expect(DB.orders).toHaveProperty(order3.orderID);
 	});
 });
