33b121334048935ae413558cde4ed55a58452488
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-05-05 13:26:26 +0100

    Tests whether the Order constructor exists and returns a result

---
+++ tests/order.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+var DB = require('../db');
+var User = require('../user');
+var Admin = require('../admin');
+var Order = require('../order');
+
+describe('Order constructor', function() {
+	it('should exist', function() {
+		expect(new Order()).toBeDefined();
+	});
+});
